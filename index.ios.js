d854a0300b4bb5fae2e85899331aa383e5cceef4
spygi <spygiannakis@hotmail.com>
2015-10-03 12:00:56 +0200

    moving server to new repo

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ var MoarBaconz = React.createClass({
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get starhajshdjkashdted, edit index.ios.js
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
