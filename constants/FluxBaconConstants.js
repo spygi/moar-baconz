c9a232218c7ae215f7ced1b1780d2da061320ff9
Jay Kid <cyph3rfox@gmail.com>
2015-10-03 12:10:28 +0200

    feat: add item states to constants

--- constants/FluxBaconConstants.js
+++ constants/FluxBaconConstants.js
@@ -4,5 +4,8 @@ var keyMirror = require('react/lib/keyMirror');
 module.exports = keyMirror({
 
   TOGGLE_AVAILABILITY: null, // Toggle availability of a specified product
+  BUY: null, // Item state (RED)
+  MAYBE_BUY: null, // Item state (YELLOW)
+  DO_NOT_BUY: null, // Item state (GREEN)
   
 });
