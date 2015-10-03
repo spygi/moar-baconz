0aacf280b28a9a315d403c8d593c85b9ca25e049
Tom Forrer <tom.forrer@gmail.com>
2015-10-03 11:08:15 +0200

    implement model and controllers
    endpoints need ids…

--- server/router.js
+++ server/router.js
@@ -1,13 +1,20 @@
 var express = require('express');
-var groupController = require('./controllers/group');
-var memberController = require('./controllers/member');
+var groupController = require('./controllers/groups');
+var memberController = require('./controllers/members');
 var router = express.Router();
 
 router.route('/group')
-  .post(groupController.postGroup);
+    .post(groupController.postGroup);
 
-router.route('/group/:group')
-  .get(groupController.getGroup)
-  .put(groupController.putGroup);
+router.route('/group/:id')
+    .get(groupController.getGroup)
+    .put(groupController.putGroup);
+
+router.route('/member')
+    .post(memberController.postMember);
+
+router.route('/member/:id')
+    .get(memberController.getMember)
+    .put(memberController.putMember);
 
 module.exports = router;
